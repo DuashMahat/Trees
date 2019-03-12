f655cff7f901ef2063c92ee528479a7636c3e22b
DuashMahat <duashmaht@hotmail.com>
2019-03-12 10:42:06 -0400

    Valid if a give tree is BST

---
+++ validateBST.cpp
@@ -0,0 +1,19 @@
+ 
+ 
+ 
+ 
+ 
+ void inorder (TreeNode* root , vector<int> & v ) {
+     if (!root ) return;
+     if (!(!root->left)) inorder ( root->left , v );
+     v.push_back(root->val);
+     if (!(!root->right)) inorder ( root->right , v );
+   }
+    
+   vector<int> inorderTraversal(TreeNode* root) {
+       // left  root and right : that is inoreder 
+        if (!root ) return {};
+        vector<int> v;
+        inorder ( root  , v);
+        return v;
+    }
